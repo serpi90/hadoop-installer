4b8a6400ec2f3165723d98eb03a941caae9bc5a9
Julian Maestri <julian@thepc.dotabase.com.ar>
2016-03-09 04:42:25 -0300

    I changed ssh to use another library, hoping it supports ecdsa key formats

--- src/main/java/hadoopInstaller/installation/Installer.java
+++ src/main/java/hadoopInstaller/installation/Installer.java
@@ -1,15 +1,5 @@
 package hadoopInstaller.installation;
 
-import hadoopInstaller.configurationGeneration.LoadFromFolder;
-import hadoopInstaller.exception.InstallationError;
-import hadoopInstaller.exception.InstallationFatalError;
-import hadoopInstaller.exception.InstallerConfigurationParseError;
-import hadoopInstaller.io.InstallerConfigurationParser;
-import hadoopInstaller.io.MD5Calculator;
-import hadoopInstaller.io.XMLDocumentReader;
-import hadoopInstaller.logging.MessageFormattingLog;
-import hadoopInstaller.util.Messages;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,12 +18,18 @@ import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.VFS;
 import org.apache.commons.vfs2.provider.sftp.SftpFileSystemConfigBuilder;
 
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
+import hadoopInstaller.configurationGeneration.LoadFromFolder;
+import hadoopInstaller.exception.InstallationError;
+import hadoopInstaller.exception.InstallationFatalError;
+import hadoopInstaller.exception.InstallerConfigurationParseError;
+import hadoopInstaller.io.InstallerConfigurationParser;
+import hadoopInstaller.io.MD5Calculator;
+import hadoopInstaller.io.XMLDocumentReader;
+import hadoopInstaller.logging.MessageFormattingLog;
+import hadoopInstaller.util.Messages;
 
 public class Installer {
-	public static final String INSTALLER_NAME = Messages
-			.getString("HadoopInstaller.InstallerName"); //$NON-NLS-1$
+	public static final String INSTALLER_NAME = Messages.getString("HadoopInstaller.InstallerName"); //$NON-NLS-1$
 
 	private boolean deploy;
 	private MessageFormattingLog log;
@@ -42,7 +38,6 @@ public class Installer {
 	private Map<String, String> fileHashes;
 	private Map<String, String> directories;
 	private FileObject configurationFilesToUpload;
-	private JSch ssh;
 	private FileSystemOptions sftpOptions;
 
 	public Installer(Log aLog, boolean doDeploy) throws InstallationFatalError {
@@ -52,37 +47,30 @@ public class Installer {
 		this.log = new MessageFormattingLog(aLog);
 		loadConfiguration();
 		configureVFS2SFTP();
-		configureJSch();
 	}
 
 	private void analyzeBundles() throws InstallationFatalError {
 		getLog().trace("HadoopInstaller.InstallationBundles.Start"); //$NON-NLS-1$
 		FileObject folder;
 		try {
-			folder = getLocalDirectory().resolveFile(
-					InstallerConstants.TGZ_BUNDLES_FOLDER);
+			folder = getLocalDirectory().resolveFile(InstallerConstants.TGZ_BUNDLES_FOLDER);
 			if (!folder.exists()) {
 				folder.createFolder();
-				getLog().warn(
-						"HadoopInstaller.InstallationBundles.FolderDoesntExist", //$NON-NLS-1$
-						InstallerConstants.TGZ_BUNDLES_FOLDER,
-						InstallerConstants.CONFIGURATION_FILE);
+				getLog().warn("HadoopInstaller.InstallationBundles.FolderDoesntExist", //$NON-NLS-1$
+						InstallerConstants.TGZ_BUNDLES_FOLDER, InstallerConstants.CONFIGURATION_FILE);
 			}
 		} catch (FileSystemException e) {
-			throw new InstallationFatalError(e,
-					"HadoopInstaller.InstallationBundles.FolderCouldNotOpen", //$NON-NLS-1$
+			throw new InstallationFatalError(e, "HadoopInstaller.InstallationBundles.FolderCouldNotOpen", //$NON-NLS-1$
 					InstallerConstants.TGZ_BUNDLES_FOLDER);
 		}
 		for (String resource : getConfig().getFiles().keySet()) {
 			String fileName = getConfig().getFiles().get(resource);
 			try {
-				getLog().trace(
-						"HadoopInstaller.InstallationBundles.From", resource, //$NON-NLS-1$
+				getLog().trace("HadoopInstaller.InstallationBundles.From", resource, //$NON-NLS-1$
 						fileName);
 				FileObject bundle = folder.resolveFile(fileName);
 				if (!bundle.exists()) {
-					throw new InstallationFatalError(
-							"HadoopInstaller.InstallationBundles.Missing", //$NON-NLS-1$
+					throw new InstallationFatalError("HadoopInstaller.InstallationBundles.Missing", //$NON-NLS-1$
 							fileName, folder.getName().getBaseName());
 				}
 				if (doDeploy()) {
@@ -96,8 +84,7 @@ public class Installer {
 							fileName);
 				}
 			} catch (FileSystemException e) {
-				throw new InstallationFatalError(e,
-						"HadoopInstaller.InstallationBundles.Error", //$NON-NLS-1$
+				throw new InstallationFatalError(e, "HadoopInstaller.InstallationBundles.Error", //$NON-NLS-1$
 						resource, fileName);
 			}
 			getLog().trace("HadoopInstaller.InstallationBundles.Success", //$NON-NLS-1$
@@ -111,46 +98,11 @@ public class Installer {
 		}
 	}
 
-	private void configureJSch() throws InstallationFatalError {
-		getLog().trace("HadoopInstaller.Configure.SSH.Start"); //$NON-NLS-1$
-		/*
-		 * TODO-- ssh-ask
-		 * 
-		 * In the case of ask, the UserInfo object should be passed later to the
-		 * SSH session.
-		 */
-		JSch.setConfig(
-				"StrictHostKeyChecking", this.configuration.getStrictHostKeyChecking() ? "yes" : "no"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		getLog().trace("HadoopInstaller.Configure.StrictHostKeyChecking", //$NON-NLS-1$
-				this.configuration.getStrictHostKeyChecking() ? "yes" : "no"); //$NON-NLS-1$//$NON-NLS-2$
-		this.ssh = new JSch();
-		try {
-			this.ssh.setKnownHosts(this.configuration.getSshKnownHosts());
-			getLog().trace("HadoopInstaller.Configure.KnownHosts", //$NON-NLS-1$
-					this.configuration.getSshKnownHosts());
-			// Set private key for SSH password-less authentication.
-			/*
-			 * TODO-- ssh-ask
-			 * 
-			 * addIdentity(String privkey, String passphrase) if the key is
-			 * password protected.
-			 */
-			this.ssh.addIdentity(getConfig().getSshKeyFile());
-			getLog().trace("HadoopInstaller.Configure.PrivateKeyFile", //$NON-NLS-1$
-					getConfig().getSshKeyFile());
-		} catch (JSchException e) {
-			throw new InstallationFatalError(e,
-					"HadoopInstaller.Configure.SSH.Fail"); //$NON-NLS-1$
-		}
-		getLog().debug("HadoopInstaller.Configure.SSH.Success"); //$NON-NLS-1$
-	}
-
 	private void configureVFS2SFTP() throws InstallationFatalError {
 		getLog().trace("HadoopInstaller.Configure.SFTP.Start"); //$NON-NLS-1$
 		FileSystemOptions options;
 		options = new FileSystemOptions();
-		SftpFileSystemConfigBuilder builder = SftpFileSystemConfigBuilder
-				.getInstance();
+		SftpFileSystemConfigBuilder builder = SftpFileSystemConfigBuilder.getInstance();
 		try {
 			builder.setUserDirIsRoot(options, false);
 			/*
@@ -159,12 +111,10 @@ public class Installer {
 			 * In the case of ask, the UserInfo object should be passed with
 			 * builder.setUserInfo()
 			 */
-			builder.setStrictHostKeyChecking(options, this.configuration
-					.getStrictHostKeyChecking() ? "yes" : "no"); //$NON-NLS-1$//$NON-NLS-2$
+			builder.setStrictHostKeyChecking(options, this.configuration.getStrictHostKeyChecking() ? "yes" : "no"); //$NON-NLS-1$//$NON-NLS-2$
 			getLog().trace("HadoopInstaller.Configure.StrictHostKeyChecking", //$NON-NLS-1$
 					this.configuration.getStrictHostKeyChecking());
-			builder.setKnownHosts(options,
-					new File(this.configuration.getSshKnownHosts()));
+			builder.setKnownHosts(options, new File(this.configuration.getSshKnownHosts()));
 			getLog().trace("HadoopInstaller.Configure.KnownHosts", //$NON-NLS-1$
 					this.configuration.getSshKnownHosts());
 			File identities[] = { new File(getConfig().getSshKeyFile()) };
@@ -178,8 +128,7 @@ public class Installer {
 			 * use setUserInfo?
 			 */
 		} catch (FileSystemException e) {
-			throw new InstallationFatalError(e,
-					"HadoopInstaller.Configure.SFTP.Fail"); //$NON-NLS-1$
+			throw new InstallationFatalError(e, "HadoopInstaller.Configure.SFTP.Fail"); //$NON-NLS-1$
 		}
 		this.sftpOptions = options;
 		getLog().debug("HadoopInstaller.Configure.SFTP.Success"); //$NON-NLS-1$
@@ -192,15 +141,12 @@ public class Installer {
 	private void generateConfigurationFiles() throws InstallationFatalError {
 		this.getLog().trace("HadoopInstaller.ConfigurationFilesToUpload.Start"); //$NON-NLS-1$
 		// TODO+ implement a new strategy that generates default files.
-		this.configurationFilesToUpload = new LoadFromFolder(
-				InstallerConstants.CONFIGURATION_FOLDER_TO_UPLOAD,
-				this.getLocalDirectory(), this.getLog())
-				.generateConfigurationFiles();
+		this.configurationFilesToUpload = new LoadFromFolder(InstallerConstants.CONFIGURATION_FOLDER_TO_UPLOAD,
+				this.getLocalDirectory(), this.getLog()).generateConfigurationFiles();
 		this.getLog().debug("HadoopInstaller.ConfigurationFilesToUpload.End"); //$NON-NLS-1$
 	}
 
-	private void getBundleHashes(FileObject file) throws FileSystemException,
-			InstallationFatalError {
+	private void getBundleHashes(FileObject file) throws FileSystemException, InstallationFatalError {
 		String fileName = file.getName().getBaseName();
 		getLog().trace("HadoopInstaller.MD5.Start", fileName); //$NON-NLS-1$
 		if (file.getType().equals(FileType.FILE)) {
@@ -210,18 +156,15 @@ public class Installer {
 				getLog().trace("HadoopInstaller.MD5.Success", fileName, //$NON-NLS-1$
 						md5);
 			} catch (NoSuchAlgorithmException | IOException e) {
-				throw new InstallationFatalError(e,
-						"HadoopInstaller.MD5.Error", fileName); //$NON-NLS-1$
+				throw new InstallationFatalError(e, "HadoopInstaller.MD5.Error", fileName); //$NON-NLS-1$
 			}
 		}
 	}
 
-	private void getBundleInstallDirectory(String resource, FileObject file)
-			throws InstallationFatalError {
+	private void getBundleInstallDirectory(String resource, FileObject file) throws InstallationFatalError {
 		getLog().trace("HadoopInstaller.InstallationDirectory.Start", resource); //$NON-NLS-1$
 		try {
-			String uri = URI.create(
-					file.getName().getURI().replaceFirst("file:", "tgz:")) //$NON-NLS-1$ //$NON-NLS-2$
+			String uri = URI.create(file.getName().getURI().replaceFirst("file:", "tgz:")) //$NON-NLS-1$ //$NON-NLS-2$
 					.toString();
 			FileObject tgzFile;
 			tgzFile = VFS.getManager().resolveFile(uri);
@@ -230,8 +173,7 @@ public class Installer {
 			getLog().debug("HadoopInstaller.InstallationDirectory.Success", //$NON-NLS-1$
 					resource, name);
 		} catch (FileSystemException e) {
-			throw new InstallationFatalError(e,
-					"HadoopInstaller.InstallationDirectory.Error", //$NON-NLS-1$
+			throw new InstallationFatalError(e, "HadoopInstaller.InstallationDirectory.Error", //$NON-NLS-1$
 					file.getName().getBaseName());
 		}
 	}
@@ -244,8 +186,7 @@ public class Installer {
 		return this.configurationFilesToUpload;
 	}
 
-	public void getConfigurationFilesToUpload(
-			FileObject someConfigurationFilesToUpload) {
+	public void getConfigurationFilesToUpload(FileObject someConfigurationFilesToUpload) {
 		this.configurationFilesToUpload = someConfigurationFilesToUpload;
 	}
 
@@ -256,20 +197,16 @@ public class Installer {
 		 */
 		try {
 			FileObject configurationSchema;
-			configurationSchema = VFS.getManager().resolveFile(
-					"ram:///" + InstallerConstants.CONFIGURATION_SCHEMA);//$NON-NLS-1$
-			try (OutputStream out = configurationSchema.getContent()
-					.getOutputStream();
-					InputStream in = this.getClass().getResourceAsStream(
-							InstallerConstants.CONFIGURATION_SCHEMA);) {
+			configurationSchema = VFS.getManager().resolveFile("ram:///" + InstallerConstants.CONFIGURATION_SCHEMA);//$NON-NLS-1$
+			try (OutputStream out = configurationSchema.getContent().getOutputStream();
+					InputStream in = this.getClass().getResourceAsStream(InstallerConstants.CONFIGURATION_SCHEMA);) {
 				while (in.available() > 0) {
 					out.write(in.read());
 				}
 			}
 			return configurationSchema;
 		} catch (IOException e) {
-			throw new InstallationFatalError(e,
-					"HadoopInstaller.Configure.CouldNotReadFile", //$NON-NLS-1$
+			throw new InstallationFatalError(e, "HadoopInstaller.Configure.CouldNotReadFile", //$NON-NLS-1$
 					InstallerConstants.CONFIGURATION_SCHEMA);
 		}
 	}
@@ -299,23 +236,17 @@ public class Installer {
 		return this.sftpOptions;
 	}
 
-	public JSch getSsh() {
-		return this.ssh;
-	}
-
 	private void loadConfiguration() throws InstallationFatalError {
 		getLog().trace("HadoopInstaller.Configure.Start", //$NON-NLS-1$
 				InstallerConstants.CONFIGURATION_FILE);
 		String localDirectoryName = System.getProperty("user.dir"); //$NON-NLS-1$
 		try {
 			setLocalDirectory(VFS.getManager().resolveFile(localDirectoryName));
-			FileObject configurationFile = getLocalDirectory().resolveFile(
-					InstallerConstants.CONFIGURATION_FILE);
+			FileObject configurationFile = getLocalDirectory().resolveFile(InstallerConstants.CONFIGURATION_FILE);
 
 			FileObject configurationSchema = getConfigurationSchema();
 			setConfig(InstallerConfigurationParser
-					.generateConfigurationFrom(XMLDocumentReader.parse(
-							configurationFile, configurationSchema)));
+					.generateConfigurationFrom(XMLDocumentReader.parse(configurationFile, configurationSchema)));
 			try {
 				configurationFile.close();
 			} catch (FileSystemException ex) {
@@ -323,12 +254,10 @@ public class Installer {
 						InstallerConstants.CONFIGURATION_FILE);
 			}
 		} catch (FileSystemException e) {
-			throw new InstallationFatalError(e,
-					"HadoopInstaller.Configure.CouldNotFindFile", //$NON-NLS-1$
+			throw new InstallationFatalError(e, "HadoopInstaller.Configure.CouldNotFindFile", //$NON-NLS-1$
 					InstallerConstants.CONFIGURATION_FILE, localDirectoryName);
 		} catch (InstallerConfigurationParseError e) {
-			throw new InstallationFatalError(e.getCause(),
-					"HadoopInstaller.Configure.CouldNotReadFile", //$NON-NLS-1$
+			throw new InstallationFatalError(e.getCause(), "HadoopInstaller.Configure.CouldNotReadFile", //$NON-NLS-1$
 					InstallerConstants.CONFIGURATION_FILE);
 		}
 		getLog().info("HadoopInstaller.Configure.Success"); //$NON-NLS-1$
@@ -349,12 +278,10 @@ public class Installer {
 				new HostInstallation(host, this).run();
 			} catch (InstallationError e) {
 				if (e.getCause() != null) {
-					getLog().error(e.getCause(),
-							"HadoopInstaller.Installation.HostFailed", //$NON-NLS-1$
+					getLog().error(e.getCause(), "HadoopInstaller.Installation.HostFailed", //$NON-NLS-1$
 							host.getHostname());
 				} else {
-					getLog().error(e,
-							"HadoopInstaller.Installation.HostFailed", //$NON-NLS-1$
+					getLog().error(e, "HadoopInstaller.Installation.HostFailed", //$NON-NLS-1$
 							host.getHostname());
 				}
 			}
--- src/main/java/hadoopInstaller/installation/SshCommandExecutor.java
+++ src/main/java/hadoopInstaller/installation/SshCommandExecutor.java
@@ -1,7 +1,5 @@
 package hadoopInstaller.installation;
 
-import hadoopInstaller.exception.ExecutionError;
-
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -10,9 +8,10 @@ import java.io.OutputStream;
 import java.util.LinkedList;
 import java.util.List;
 
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
+import org.apache.commons.io.IOUtils;
+
+import ch.ethz.ssh2.Session;
+import hadoopInstaller.exception.ExecutionError;
 
 public class SshCommandExecutor {
 
@@ -68,33 +67,25 @@ public class SshCommandExecutor {
 	 */
 	public void execute(String command) throws ExecutionError {
 		clearBuffers();
-		ChannelExec channel = openExecChannel();
-		channel.setInputStream(null);
-		channel.setCommand(command);
-		channel.setErrStream(this.error);
 		try {
-			channel.connect();
-			BufferedReader consoleReader = new BufferedReader(
-					new InputStreamReader(channel.getInputStream()));
-			while (!channel.isClosed() || consoleReader.ready()) {
+			session.execCommand(command);
+			BufferedReader consoleReader = new BufferedReader(new InputStreamReader(session.getStdout()));
+			String line;
+			do {
 				if (!consoleReader.ready()) {
 					doWait();
 				}
-				output(consoleReader.readLine());
-			}
-		} catch (JSchException | IOException e) {
-			throw new ExecutionError(e,
-					"SshCommandExecutor.ErrorWhileExecuting", command); //$NON-NLS-1$
-		} finally {
-			if (channel.isConnected()) {
-				channel.disconnect();
-			}
+				line = consoleReader.readLine();
+				output(line);
+			} while (line == null);
+			IOUtils.copy(session.getStderr(), this.error);
+		} catch (IOException e) {
+			throw new ExecutionError(e, "SshCommandExecutor.ErrorWhileExecuting", command); //$NON-NLS-1$
 		}
-		this.exitStatus = channel.getExitStatus();
-		if (channel.getExitStatus() != 0) {
-			throw new ExecutionError(
-					"SshCommandExecutor.CommandReturnedStatus", command, //$NON-NLS-1$
-					channel.getExitStatus());
+		this.exitStatus = session.getExitStatus();
+		if (this.exitStatus != 0) {
+			throw new ExecutionError("SshCommandExecutor.CommandReturnedStatus", command, //$NON-NLS-1$
+					this.exitStatus);
 		}
 	}
 
@@ -108,17 +99,6 @@ public class SshCommandExecutor {
 
 	public List<String> getOutput() {
 		return this.output;
-	}
-
-	private ChannelExec openExecChannel() throws ExecutionError {
-		ChannelExec channel;
-		try {
-			channel = (ChannelExec) this.session.openChannel("exec"); //$NON-NLS-1$
-		} catch (JSchException e) {
-			throw new ExecutionError(e, "SshCommandExecutor.ErrorConnectingTo", //$NON-NLS-1$
-					this.session.getHost());
-		}
-		return channel;
 	}
 
 	private void output(String line) {
